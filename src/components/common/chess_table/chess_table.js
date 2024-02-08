bd51186bd1580176aa5a04fe3f21c2aeec394c7c
GabrielNatucci <gabriel.natuccirusso@hotmail.com>
2024-02-08 12:42:51 -0300

    O tabuleiro tá tudo bugado

--- src/components/common/chess_table/chess_table.js
+++ src/components/common/chess_table/chess_table.js
@@ -3,59 +3,6 @@ import React from "react";
 import "./styles.css";
 import Square from "../Square/Square";
 
-let vertical = ["1", "2", "3", "4", "5", "6", "7", "8"];
-let horizontal = ["a", "b", "c", "d", "e", "f", "g", "h"];
-let table = [];
-let pieces_table = [
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""]
-];
-
-pieces_table[0][0] = "wrook";
-pieces_table[1][0] = "wknight";
-pieces_table[2][0] = "wbishop";
-pieces_table[3][0] = "wqueen";
-pieces_table[4][0] = "wking";
-pieces_table[5][0] = "wbishop";
-pieces_table[6][0] = "wknight";
-pieces_table[7][0] = "wrook";
-
-for (let i = 0; i < 8; i++) {
-    pieces_table[i][1] = "wpawn";
-}
-
-pieces_table[0][7] = "brook";
-pieces_table[1][7] = "bknight";
-pieces_table[2][7] = "bbishop";
-pieces_table[3][7] = "bqueen";
-pieces_table[4][7] = "bking";
-pieces_table[5][7] = "bbishop";
-pieces_table[6][7] = "bknight";
-pieces_table[7][7] = "brook";
-
-for (let i = 0; i < 8; i++) {
-    pieces_table[i][6] = "bpawn";
-}
-
-for (let j = vertical.length - 1; j >= 0; j--) {
-    for (let i = 0; i < horizontal.length; i++) {
-        table.push(<Square
-            key={`${i} + ${j}`}
-            Row={j}
-            Column={i}
-            Text={`${horizontal[i]}${vertical[j]}`}
-        >
-            <Piece type={pieces_table[i][j]} file={i + 1} rank={j + 1} />
-        </Square >);
-    }
-}
-
 class ChessTable extends React.Component {
     constructor(props) {
         super(props);
@@ -63,6 +10,21 @@ class ChessTable extends React.Component {
     }
 
     render() {
+        for (let j = this.props.vertical.length - 1; j >= 0; j--) {
+            for (let i = 0; i < this.props.horizontal.length; i++) {
+                this.props.table.push(<Square
+                    key={`${i} + ${j}`}
+                    Row={j}
+                    Column={i}
+                    Text={`${this.props.horizontal[i]}${this.props.vertical[j]}`}
+                >
+                    <Piece type={this.props.pieces_table[i][j]} file={i + 1} rank={j + 1} />
+                </Square >);
+            }
+        }
+
+        console.log(this.props.table);
+
         return (
             <div
                 className="chess-table"
@@ -70,7 +32,7 @@ class ChessTable extends React.Component {
                 onMouseDown={this.props.grabPiece}
                 onMouseMove={this.props.movePiece}
             >
-                {table}
+                {this.props.table}
             </div>
         )
 
--- src/pages/chess_game/index.js
+++ src/pages/chess_game/index.js
@@ -2,6 +2,46 @@ import React from "react"
 import ChessTable from "../../components/common/chess_table/chess_table";
 import "./styles.css"
 
+let vertical = ["1", "2", "3", "4", "5", "6", "7", "8"];
+let horizontal = ["a", "b", "c", "d", "e", "f", "g", "h"];
+let table = [];
+let pieces_table = [
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""]
+];
+
+pieces_table[0][0] = "wrook";
+pieces_table[1][0] = "wknight";
+pieces_table[2][0] = "wbishop";
+pieces_table[3][0] = "wqueen";
+pieces_table[4][0] = "wking";
+pieces_table[5][0] = "wbishop";
+pieces_table[6][0] = "wknight";
+pieces_table[7][0] = "wrook";
+
+for (let i = 0; i < 8; i++) {
+    pieces_table[i][1] = "wpawn";
+}
+
+pieces_table[0][7] = "brook";
+pieces_table[1][7] = "bknight";
+pieces_table[2][7] = "bbishop";
+pieces_table[3][7] = "bqueen";
+pieces_table[4][7] = "bking";
+pieces_table[5][7] = "bbishop";
+pieces_table[6][7] = "bknight";
+pieces_table[7][7] = "brook";
+
+for (let i = 0; i < 8; i++) {
+    pieces_table[i][6] = "bpawn";
+}
+
 class ChessGame extends React.Component {
     constructor(props) {
         super(props);
@@ -17,7 +57,6 @@ class ChessGame extends React.Component {
         function grabPiece(e) {
             if (e.target.classList.contains("piece")) { // verifica se eh uma peça
                 active_piece.current = e.target;
-                console.log(active_piece.current.style.position)
             }
         }
 
@@ -71,6 +110,10 @@ class ChessGame extends React.Component {
                 <ChessTable
                     grabPiece={grabPiece}
                     movePiece={movePiece}
+                    horizontal={horizontal}
+                    vertical={vertical}
+                    table={table}
+                    pieces_table={pieces_table}
                 />
             </ main>
         )

dfdd946da01803889495f74a0606039e22b292e0
GabrielNatucci <gabriel.natuccirusso@hotmail.com>
2024-02-08 14:42:35 -0300

    Agora consegue ver em qual quadrado tá caindo
    - fiz algumas mudanças para o programa conseguir perceber em qual quadrado a peça tá sendo solta

--- src/components/common/chess_table/chess_table.js
+++ src/components/common/chess_table/chess_table.js
@@ -1,42 +1,18 @@
-import Piece from "../pieces/Piece.js";
-import React from "react";
 import "./styles.css";
-import Square from "../Square/Square";
 
-class ChessTable extends React.Component {
-    constructor(props) {
-        super(props);
-        this.RefForPiece = React.createRef();
-    }
+const ChessTable = (props) => {
 
-    render() {
-        for (let j = this.props.vertical.length - 1; j >= 0; j--) {
-            for (let i = 0; i < this.props.horizontal.length; i++) {
-                this.props.table.push(<Square
-                    key={`${i} + ${j}`}
-                    Row={j}
-                    Column={i}
-                    Text={`${this.props.horizontal[i]}${this.props.vertical[j]}`}
-                >
-                    <Piece type={this.props.pieces_table[i][j]} file={i + 1} rank={j + 1} />
-                </Square >);
-            }
-        }
+    return (
+        <div
+            className="chess-table"
+            id="chess-table-id"
+            onMouseDown={props.grabPiece}
+            onMouseMove={props.movePiece}
+        >
+            {props.table}
+        </div>
+    )
 
-        console.log(this.props.table);
-
-        return (
-            <div
-                className="chess-table"
-                id="chess-table-id"
-                onMouseDown={this.props.grabPiece}
-                onMouseMove={this.props.movePiece}
-            >
-                {this.props.table}
-            </div>
-        )
-
-    }
 }
 
 export default ChessTable
--- src/pages/chess_game/index.js
+++ src/pages/chess_game/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import ChessTable from "../../components/common/chess_table/chess_table";
 import "./styles.css"
+import Square from "../../components/common/Square/Square";
+import Piece from "../../components/common/pieces/Piece"
 
 let vertical = ["1", "2", "3", "4", "5", "6", "7", "8"];
 let horizontal = ["a", "b", "c", "d", "e", "f", "g", "h"];
-let table = [];
 let pieces_table = [
     ["", "", "", "", "", "", "", ""],
     ["", "", "", "", "", "", "", ""],
@@ -42,6 +43,20 @@ for (let i = 0; i < 8; i++) {
     pieces_table[i][6] = "bpawn";
 }
 
+let table = [];
+for (let j = vertical.length - 1; j >= 0; j--) {
+    for (let i = 0; i < horizontal.length; i++) {
+        table.push(<Square
+            key={`${i} + ${j}`}
+            Row={j}
+            Column={i}
+            Text={`${horizontal[i]}${vertical[j]}`}
+        >
+            <Piece type={pieces_table[i][j]} file={i + 1} rank={j + 1} />
+        </Square >);
+    }
+}
+
 class ChessGame extends React.Component {
     constructor(props) {
         super(props);
@@ -49,12 +64,14 @@ class ChessGame extends React.Component {
     }
 
     render() {
-        let TableItSelf = null
         let active_piece = this.RefForPiece;
         var r = document.querySelector(':root');
         var rs = getComputedStyle(r);
 
+        let TableItSelf = null;
+
         function grabPiece(e) {
+            TableItSelf = document.getElementById('chess-table-id');
             if (e.target.classList.contains("piece")) { // verifica se eh uma peça
                 active_piece.current = e.target;
             }
@@ -62,8 +79,6 @@ class ChessGame extends React.Component {
 
         function movePiece(e) {
             if (active_piece.current) {
-                TableItSelf = document.getElementById('chess-table-id');
-
                 let offsetTop = TableItSelf.offsetTop;
                 let offsetRight = parseInt(TableItSelf.getBoundingClientRect().right);
                 let offsetLeft = TableItSelf.offsetLeft;
@@ -95,8 +110,38 @@ class ChessGame extends React.Component {
             }
         }
 
-        function letOffPiece() {
+        function letOffPiece(e) {
             if (active_piece.current) {
+                let offsetRight = parseInt(TableItSelf.getBoundingClientRect().right);
+                let offsetLeft = TableItSelf.offsetLeft;
+
+                let offsetTop = TableItSelf.offsetTop;
+                let offsetBottom = parseInt(TableItSelf.getBoundingClientRect().bottom);
+
+                let EachRowSquare = ((offsetRight - offsetLeft) / 8);
+                let EachFileSquare = ((offsetTop - offsetBottom) / 8);
+
+                let end_square = [];
+
+                for (let i = 0; i < 8; i++) { // sees with row the piece is being dropped on
+                    if (e.clientX > (offsetLeft + (i * EachRowSquare)) && e.clientX < (offsetLeft + ((i + 1) * EachRowSquare))) {
+                        end_square.push(i);
+                        break;
+                    }
+                }
+
+                for (let i = 0; i < 8; i++) { // sees with file the piece is being dropped on
+                    if (e.clientY > (offsetBottom + ((i + 1) * EachFileSquare))
+                        && e.clientY > (offsetBottom + ((i + 2) * EachFileSquare))) {
+                        end_square.push(i);
+                        break;
+                    }
+                }
+
+                console.log(end_square);
+
+                pieces_table[end_square[0]][end_square[1]] = "bqueen";
+
                 active_piece.current.style.position = "";
                 active_piece.current = null;
             }

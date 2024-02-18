33ce9fd1b9a636cec926f99e7a546deffced270e
GabrielNatucci <gabriel.natuccirusso@hotmail.com>
2024-02-18 16:51:16 -0300

    tirei mais dois console.log

--- src/pages/chess_game/functions/evaluate/com/areSameColors.js
+++ src/pages/chess_game/functions/evaluate/com/areSameColors.js
@@ -3,9 +3,6 @@ const areSameColor = (
     first,
     second,
 ) => {
-    console.log(pieces_table[first[0]][first[1]][0]);
-    console.log(pieces_table[second[0]][second[1]][0]);
-
     if (pieces_table[first[0]][first[1]][0] === pieces_table[second[0]][second[1]][0])
         return true;
 

e934fa73d4871b6debc2bc8c168f4ca9ed9b6269
GabrielNatucci <gabriel.natuccirusso@hotmail.com>
2024-02-19 09:21:03 -0300

    Peões pretos feitos
    - Regra dos peões pretos feita
    - Correção em um buraco na regra do peão branco

--- src/pages/chess_game/functions/evaluate/evaluateBlackPawn.js
+++ src/pages/chess_game/functions/evaluate/evaluateBlackPawn.js
@@ -1,28 +1,25 @@
 import areSameColor from "./com/areSameColors";
 import isThereAPiece from "./com/isThereAPiece";
 
-const evaluateWhitePawn = (
+const evaluateBlackPawn = (
     pieces_table,
     start_square,
     end_square,
 ) => {
-    const origin_square = 1;
+    const origin_square = 6;
 
-    if (end_square[1] - start_square[1] > 0) {
+    if (end_square[1] - start_square[1] < 0) {
         if (start_square[0] === end_square[0]) { // caso o movimento seja andando pra frente
             // caso não tenha nenhuma peça casa que a peça vai ficar
             if (isThereAPiece(pieces_table, end_square) === false) {
                 // caso a peça estega na casa de origem e
                 // caso o movimento seja até dois pra frente
-                if (start_square[1] === origin_square &&
-                    (end_square[1] - start_square[1] <= 2)) {
+                if (start_square[1] === origin_square && (end_square[1] - start_square[1] >= -2))
                     return true;
-                }
 
                 // caso seja só um lance pra frente
-                if (end_square[1] - start_square[1] <= 1) {
+                if (end_square[1] - start_square[1] >= -1)
                     return true;
-                }
             }
         } else {
             // verifica se é uma tentativa de movimento na diagonal
@@ -40,4 +37,4 @@ const evaluateWhitePawn = (
     return false;
 };
 
-export default evaluateWhitePawn;
+export default evaluateBlackPawn;
--- src/pages/chess_game/functions/validade_moves.js
+++ src/pages/chess_game/functions/validade_moves.js
@@ -3,6 +3,7 @@ import evaluateRook from "./evaluate/evaluateRook.js";
 import evaluateKnight from "./evaluate/evaluateKnight.js";
 import evaluateKing from "./evaluate/evaluateKing.js";
 import evaluateBishop from "./evaluate/evaluateBishop.js";
+import evaluateBlackPawn from "./evaluate/evaluateBlackPawn.js";
 
 const validadeMoves = (
     pieces_table,
@@ -11,7 +12,6 @@ const validadeMoves = (
     active_piece,
     isBlackToMove,
 ) => {
-    // console.log(active_piece.current);
     // feitos: 
     // peoes brancos
     // torres
@@ -19,13 +19,13 @@ const validadeMoves = (
     // reis
     // bispos
     // movimentos alternados, cada um tem sua vez afinal de contas
-    //
-    // falta:
-    // dama
     // peões pretos
+
+    // falta:
     // an passant
+    // dama
     // roque
-    // refatorar torres
+    // refatorar torres | ESSE AQ EH IMPORTANTE FAZER
 
     // movimentos alternados, cada um tem sua vez afinal de contas
     if (isBlackToMove.current === true) {
@@ -37,47 +37,52 @@ const validadeMoves = (
     }
 
     if (active_piece.current.className.search("wpawn") === 0) { //peões brancos
-        if (evaluateWhitePawn(pieces_table, start_square, end_square) === true) {
+        if (evaluateWhitePawn(pieces_table, start_square, end_square) === true)
             return true;
-        } else {
+        else
             return false;
-        }
+    }
+
+    if (active_piece.current.className.search("bpawn") === 0) { //peões pretos
+        if (evaluateBlackPawn(pieces_table, start_square, end_square) === true)
+            return true;
+        else
+            return false;
+
     }
 
     if (active_piece.current.className.search("wrook") === 0 || // torres
         active_piece.current.className.search("brook") === 0) {
-        if (evaluateRook(pieces_table, start_square, end_square) === true) {
+        if (evaluateRook(pieces_table, start_square, end_square) === true)
             return true;
-        } else {
+        else
             return false;
-        }
     }
 
     if (active_piece.current.className.search("wknight") === 0 || // cavalos
         active_piece.current.className.search("bknight") === 0) {
-        if (evaluateKnight(pieces_table, start_square, end_square) === true) {
+        if (evaluateKnight(pieces_table, start_square, end_square) === true)
             return true;
-        } else {
+        else
             return false;
-        }
+
     }
 
     if (active_piece.current.className.search("wking") === 0 || // reis
         active_piece.current.className.search("bking") === 0) {
-        if (evaluateKing(pieces_table, start_square, end_square)) {
+        if (evaluateKing(pieces_table, start_square, end_square))
             return true;
-        } else {
+        else
             return false;
-        }
     }
 
     if (active_piece.current.className.search("wbishop") === 0 || // bispos
         active_piece.current.className.search("bbishop") === 0) {
-        if (evaluateBishop(pieces_table, start_square, end_square)) {
+        if (evaluateBishop(pieces_table, start_square, end_square))
             return true;
-        } else {
+        else
             return false;
-        }
+
     }
 
     return true;

b85d6e6a831074c1044a8dcd7fa1e20f5085019f
GabrielNatucci <gabriel.natuccirusso@hotmail.com>
2024-02-23 09:32:27 -0300

    Move change

--- src/components/common/move/move.js
+++ src/components/common/move/move.js
@@ -2,7 +2,15 @@ import './move.css'
 
 const Move = ({move, count}) => {
     return (
-        <p>{count}. {move}</p>
+            <div className="move_class d-flex">
+                <div className="mov_count p-1 m-1 d-flex justify-content-center items-align-center">
+                    {count}.
+                </div>
+
+                <div className="move_itself p-1 m-1 d-flex justify-content-center items-align-center">
+                    {move}
+                </div>
+            </div>
     )
 }
 
